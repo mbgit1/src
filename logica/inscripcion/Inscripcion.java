eb83f740029b9e1f635c9ec9890f41776755de4c
gnovasco <34897429+gnovasco@users.noreply.github.com>
2018-02-23 22:54:02 -0300

    Add files via upload

--- logica/inscripcion/Inscripcion.java
+++ logica/inscripcion/Inscripcion.java
@@ -1,5 +1,52 @@
 package logica.inscripcion;
 
 public class Inscripcion {
+	int numero;
+	int anioLectivo;
+	int montoBase;
+	int calificacion;
+	String codigoAsignatura;
+	
+	public Inscripcion( int numero, int anioLectivo, int montoBase, String codigoAsignatura ) {
+		this.numero				= numero;
+		this.anioLectivo		= anioLectivo;
+		this.montoBase			= montoBase;
+		this.calificacion		= 0;
+		this.codigoAsignatura	= codigoAsignatura;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public int getAnio() {
+		return anioLectivo;
+	}	
+	
+	public int getMonto() {
+		return montoBase;
+	}	
 
+	public int getCalificacion() {
+		return calificacion;
+	}
+	
+	public String getCodigoAsignatura() {
+		return codigoAsignatura;
+	}		
+	
+	public void setCalificacion(int calificacion) {
+		this.calificacion = calificacion;
+	}
+	
+	public boolean aprobada() {
+		return calificacion >= 6;
+	}	
+
+	//public boolean inscripcionActiva(int anioLectivo) {}		
+	
+	public boolean calificada() {
+		return calificacion != 0;
+	}	
+	
 }
--- logica/inscripcion/Inscripciones.java
+++ logica/inscripcion/Inscripciones.java
@@ -1,5 +1,74 @@
 package logica.inscripcion;
 
-public class Inscripciones {
+import java.util.LinkedList;
+import java.util.List;
 
+import logica.inscripcion.Inscripcion;
+import logica.vo.VOInscripcion;
+
+public class Inscripciones {
+	LinkedList<Inscripcion> inscripciones;
+	
+	public Inscripciones() {
+		inscripciones = new LinkedList<Inscripcion>();
+	}	
+	
+	public void addInscripcion( Inscripcion inscripcion ) {
+		inscripciones.add( inscripcion );
+	}	
+	
+	public List<VOInscripcion> listarInscripciones() {
+		List<VOInscripcion> lista = new LinkedList<VOInscripcion>();
+
+		for(Inscripcion i: inscripciones) {
+			lista.add( new VOInscripcion( i.numero, i.codigoAsignatura, i.anioLectivo, i.montoBase ) );
+		}
+		
+		return lista;
+	}	
+	
+	public Inscripcion obtenerInscripcion( int numero ) { 
+		
+		return inscripciones.get(numero);
+	}
+	
+	public int ultimaInscripcion() {
+		return inscripciones.getLast().numero;
+	}
+	
+	public void registrarInscripcion(int numero, int calificacion) {
+		inscripciones.get(numero).setCalificacion(calificacion);
+	}	
+	
+	public boolean calificada(int numero) {
+		return inscripciones.get(numero).calificada();
+	}	
+
+	public int montoRecaudado(int anioLectivo) {
+		int monto = 0;
+		
+		for(Inscripcion i: inscripciones) {
+			if (i.anioLectivo == anioLectivo)
+				monto+= i.montoBase;
+		}
+		
+		return monto;
+	}
+
+	public boolean egresado() {
+		int cantAprobadas = 0;
+		
+		for(Inscripcion i: inscripciones) {
+			if (i.aprobada())
+				cantAprobadas++;
+		}
+		
+		return cantAprobadas == 10;
+	}	
+
+	public boolean validarInscripcion(String codigoAsignatura, int anioLectivo) {
+		//terminar. //mirar .7 requerimiento verificaciones
+		return false;
+	}	
+	
 }
