8080a1465366c2eaa7f83d80db5d6afa7bbc6e5c
pelusa <guille.osores@gmail.com>
2018-02-25 16:40:11 -0300

    se quitan excepciones y se arrojan las de java

--- configuracion/Configuracion.java
+++ configuracion/Configuracion.java
@@ -5,27 +5,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-import exception.ConfiguracionException;
-
 public final class Configuracion {
 
 	private static final String RUTA_ARCHIVO_CONFIG = "config/config.properties";
 	
 	private Configuracion() {}
 	
-	public static String getProperty(String nombre) throws ConfiguracionException {
+	public static String getProperty(String nombre) throws FileNotFoundException, IOException {
 		
 		Properties configuracion = new Properties();
-		try {
-			configuracion.load (new FileInputStream (RUTA_ARCHIVO_CONFIG));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			throw new ConfiguracionException("No se encontró el archivo");
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new ConfiguracionException("Error al cargar el archivo");
-		}
-		
+		configuracion.load (new FileInputStream (RUTA_ARCHIVO_CONFIG));
 		String propiedad = configuracion.getProperty(nombre);
 		
 		return propiedad;
