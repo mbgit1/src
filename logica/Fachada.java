41cdc57f34f7d0b141d1c6071c9a0a463b157df7
pelusa <guille.osores@gmail.com>
2018-02-25 16:31:41 -0300

    agregue todos lo metodos

--- logica/Fachada.java
+++ logica/Fachada.java
@@ -1,14 +1,24 @@
 package logica;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
 
 import configuracion.Configuracion;
-import exception.ConfiguracionException;
-import exception.PersistenciaException;
+import exception.ListaLlenaException;
+import logica.alumno.Alumno;
 import logica.alumno.Alumnos;
+import logica.asignatura.Asignatura;
 import logica.asignatura.Asignaturas;
-import logica.vo.VOFachadaPersistencia;
+import logica.vo.VOAlumno;
+import logica.vo.VOAlumnoListado;
+import logica.vo.VOAlumnoModificar;
+import logica.vo.VOAsignatura;
+import logica.vo.VOBecado;
+import logica.vo.VOEgresado;
+import logica.vo.VOEscolaridad;
+import logica.vo.VOFachada;
 import persistencia.Persistencia;
 import logica.IFachada;
 import logica.Monitor;
@@ -22,55 +32,81 @@ public class Fachada extends UnicastRemoteObject implements IFachada {
 		private Alumnos alumnos;
 		private Monitor monitor;
 		
-		public Fachada() throws RemoteException {
-			asignaturas = new Asignaturas();
-			alumnos = new Alumnos();
-			monitor = new Monitor();
-		}
-
-		@Override
-		public void respaldar() throws RemoteException, PersistenciaException, ConfiguracionException {
-
-			VOFachadaPersistencia voFachada;
-			String nombreArchivo = Configuracion.getProperty("archivorRespaldo");
-			Persistencia persistencia = new Persistencia();
-			
-			monitor.comienzoLectura();
-			
-			voFachada = new VOFachadaPersistencia(asignaturas, alumnos);
-			
+		public Fachada() throws RemoteException, IOException, ClassNotFoundException {
+			VOFachada voFachadaPersistencia;
 			try {
-				persistencia.respaldar(nombreArchivo,voFachada);
-			} catch (PersistenciaException e) {
-				monitor.terminoLectura();
-				throw e;
-			}
+				voFachadaPersistencia = Persistencia.recuperar( Configuracion.getProperty("ArchivoRespaldo") );
 			
-			monitor.terminoLectura();
+				asignaturas = new Asignaturas( voFachadaPersistencia.getAsignaturas() );
+				alumnos = new Alumnos( voFachadaPersistencia.getAlumnos() );
+				monitor = new Monitor();
+			} catch (ClassNotFoundException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
-		@Override
-		public void recuperar() throws RemoteException, PersistenciaException, ConfiguracionException {
-			
-			VOFachadaPersistencia voFachada = obtenerArchivoRecuperacion();
-			
+		public void respaldar() throws RemoteException, IOException {
 			monitor.comienzoEscritura();
 			
-			asignaturas = voFachada.getAsignaturas();
-			alumnos = voFachada.getAlumnos();
+			VOFachada voFachada = new VOFachada( asignaturas, alumnos );
+			Persistencia.respaldar( Configuracion.getProperty("ArchivoRespaldo"), voFachada );
 			
 			monitor.terminoEscritura();
 		}
 		
-		private VOFachadaPersistencia obtenerArchivoRecuperacion() throws PersistenciaException, ConfiguracionException {
+		public void registrarAsignatura( VOAsignatura voAsignatura) throws ListaLlenaException {
+			Asignatura asignatura = new Asignatura( voAsignatura.getCodigo(), voAsignatura.getNombre(), voAsignatura.getDescripcion() );
+			if( !asignaturas.estaLlena() ) {
+				asignaturas.addAsignatura( asignatura );
+			}
+		}
+		
+		public void registrarAlumno( VOAlumno voAlumno ){
+			Alumno alumno = new Alumno( voAlumno.getCedula(), voAlumno.getNombre(), voAlumno.getApellido(), voAlumno.getDomicilio(), voAlumno.getTelefono(), voAlumno.getEmail() );
+			/*if( !alumnos.contiene( alumno.getCedula() ) ) {
+				alumnos.poner( alumno.getCedula(), alumno);
+			}*/
+		}
+		
+		public void registrarBecado( VOBecado voBecado ) {
 			
-			VOFachadaPersistencia voFachada;
-			Persistencia persistencia = new Persistencia();
-			String nombreArchivo = Configuracion.getProperty("archivoRespaldo");
+		}
+		
+		public void modificarAlumno( VOAlumnoModificar voAlumnoModificar ){
 			
-			voFachada = persistencia.recuperar(nombreArchivo);
+		}
+		
+		public List<VOAsignatura> listarAsignaturas(){//revisar si no deberia devolver un vo especifico para el listado
+			return null;
+		}
+		
+		public List<VOAlumnoListado> listarAlumnos( String apellido ){
+			return null;
+		}
+		
+		public List<VOAlumno> listadoDetalleAlumnos( int noMeAcuerdo ){
+			return null;
+		}
+		
+		public void inscripcionAsignatura( String niIdea1, int niIdea2, VOAlumnoListado voAlumnoListado ) {
 			
-			return voFachada;
+		}
+		
+		public void registrarCalificacion( int cedula, int niIdea, int nota ) {
+			
+		}
+		
+		public int montoRecaudado( int cedula, int niIdea ) {
+			return 1;
+		}
+		
+		public List<VOEscolaridad> escolaridad( int cedula, boolean listadCorto ){
+			return null;
+		}
+		
+		public List<VOEgresado> listadoEgresados( boolean listadoCorto ){
+			return null;
 		}
 		
 }
--- logica/IFachada.java
+++ logica/IFachada.java
@@ -1,21 +1,50 @@
 package logica;
 
-import java.math.BigDecimal;
+import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
-import exception.ConfiguracionException;
+import exception.ListaLlenaException;
 import exception.PersistenciaException;
+import logica.vo.VOAlumno;
+import logica.vo.VOAlumnoListado;
+import logica.vo.VOAlumnoModificar;
+import logica.vo.VOAsignatura;
+import logica.vo.VOBecado;
+import logica.vo.VOEgresado;
+import logica.vo.VOEscolaridad;
 
 
 
 public interface IFachada extends Remote {
 	
+	public void registrarAsignatura( VOAsignatura voAsignatura) throws RemoteException, ListaLlenaException ;
 	
-	public void respaldar() throws PersistenciaException, RemoteException, ConfiguracionException;
+	public void registrarAlumno( VOAlumno voAlumno ) throws RemoteException;
+	
+	public void registrarBecado( VOBecado voBecado ) throws RemoteException;
+	
+	public void modificarAlumno( VOAlumnoModificar voAlumnoModificar ) throws RemoteException;
+	
+	public List<VOAsignatura> listarAsignaturas() throws RemoteException; //revisar si no deberia devolver un vo especifico para el listado
+	
+	public List<VOAlumnoListado> listarAlumnos( String apellido ) throws RemoteException;
+	
+	public List<VOAlumno> listadoDetalleAlumnos( int noMeAcuerdo ) throws RemoteException;
+	
+	public void inscripcionAsignatura( String niIdea1, int niIdea2, VOAlumnoListado voAlumnoListado ) throws RemoteException;
+	
+	public void registrarCalificacion( int cedula, int niIdea, int nota ) throws RemoteException;
+	
+	public int montoRecaudado( int cedula, int niIdea ) throws RemoteException;
+	
+	public List<VOEscolaridad> escolaridad( int cedula, boolean listadCorto ) throws RemoteException;
+	
+	public List<VOEgresado> listadoEgresados( boolean listadoCorto ) throws RemoteException;
+	
+	public void respaldar()  throws RemoteException, PersistenciaException, IOException;
 
-	public void recuperar() throws PersistenciaException, RemoteException, ConfiguracionException;
 
 /*	
 	public void registrarBus(VOBusEntrada voBus)
