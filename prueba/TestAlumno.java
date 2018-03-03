8c5e44371fd77660c30389632b51f05f3cff86aa
gnovasco <gatutus@gmail.com>
2018-03-03 15:48:25 -0300

    se agregan pruebas

--- prueba/TestAlumno.java
+++ prueba/TestAlumno.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import logica.alumno.Alumno;
 import logica.alumno.Becado;
+import logica.asignatura.Asignatura;
+import logica.inscripcion.Inscripcion;
+import logica.inscripcion.Inscripciones;
 import logica.alumno.Alumnos;
 import logica.vo.VOAlumnoListado;
+import logica.vo.VOEgresado;
 import logica.vo.VOEscolaridad;
 
 
@@ -71,6 +75,78 @@ public class TestAlumno {
 			
 		}		
 
+		System.out.println("\nAgregamos 10 asignaturas aprobadas para el alumno (40000100 - Osores)...\n");		
+		//
+		
+		Asignatura as1  = new Asignatura("as1", "Asignatura 1", "Desc As 1");
+		Asignatura as2  = new Asignatura("as2", "Asignatura 2", "Desc As 2");
+		Asignatura as3  = new Asignatura("as3", "Asignatura 3", "Desc As 3");
+		Asignatura as4  = new Asignatura("as4", "Asignatura 4", "Desc As 4");
+		Asignatura as5  = new Asignatura("as5", "Asignatura 5", "Desc As 5");
+		Asignatura as6  = new Asignatura("as6", "Asignatura 6", "Desc As 6");
+		Asignatura as7  = new Asignatura("as7", "Asignatura 7", "Desc As 7");
+		Asignatura as8  = new Asignatura("as8", "Asignatura 8", "Desc As 8");
+		Asignatura as9  = new Asignatura("as9", "Asignatura 9", "Desc As 9");
+		Asignatura as10 = new Asignatura("as10", "Asignatura 10", "Desc As 10");
+
+		Inscripciones ins = alumno1.getInscripciones();	
+		int idIns = alumno1.ultimaInscripcion();		
+				
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as1));				
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as2));						
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as3));						
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as4));						
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as5));						
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as6));						
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as7));						
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as8));							
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as9));						
+		alumno1.addInscripcion(new Inscripcion(++idIns, 2015, 1000, as10));		
+
+		ins.registrarCalificacion(1, 10);
+		ins.registrarCalificacion(2, 10);
+		ins.registrarCalificacion(3, 12);	
+		ins.registrarCalificacion(4, 8);		
+		ins.registrarCalificacion(5, 8);		
+		ins.registrarCalificacion(6, 9);
+		ins.registrarCalificacion(7, 11);		
+		ins.registrarCalificacion(8, 10);
+		ins.registrarCalificacion(9, 10);
+		ins.registrarCalificacion(10, 12);		
+		
+		//
+		System.out.println("Promedio de aprobación para el alumno (40000100)...\n");
+		
+		float promedioTotal = alumnos.promedioAprobacion(40000100, true);
+		
+		System.out.println("El promedio de aprobación total para el alumno "+alumnos.obtenerAlumno(40000100).getApellido()+" es de: "+promedioTotal+"\n");
+
+		float promedioParcial = alumnos.promedioAprobacion(40000100, false);
+		
+		System.out.println("El promedio de aprobación parcial para el alumno "+alumnos.obtenerAlumno(40000100).getApellido()+" es de: "+promedioParcial+"\n");		
+
+		
+		System.out.println("\nListado (parcial) de egresados...\n");
+		
+		List<VOEgresado> egresadosParcial = alumnos.listadoEgresados (true);		
+
+		for(VOEgresado voe: egresadosParcial) {
+			
+			System.out.println("Nombre: "+voe.getNombre()+" "+voe.getApellido()+", Cedula: "+voe.getCedula());
+			
+		}		
+
+		System.out.println("\nListado (completo) de egresados...\n");
+		
+		List<VOEgresado> egresadosCompleto = alumnos.listadoEgresados (false);		
+
+		for(VOEgresado voe: egresadosCompleto) {
+			
+			System.out.println("Nombre: "+voe.getNombre()+" "+voe.getApellido()+", Cedula: "+voe.getCedula()+", Prom Calif. "+voe.getPromedioCalificacion()+", Prom Aprob.: "+voe.getPromedioAprobacion());
+			
+		}		
+
+		
 	}
 
 }
